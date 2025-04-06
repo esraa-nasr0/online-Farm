e1307eee9002df7cf60e24aec44d299cb1f8d212
esraa-nasr0 <esraaelbordeny@gmail.com>
2025-04-06 19:26:00 +0200

    new update

--- src/Context/Locationshedcontext.js
+++ src/Context/Locationshedcontext.js
@@ -14,51 +14,7 @@ const getHeaders = () => {
     return {
         Authorization: formattedToken
     };
-  };
-
-// function getTreatment(page, limit, filters = {}) {
-//     const headers = getHeaders(); 
-//     return axios
-//         .get(`https://farm-project-bbzj.onrender.com/api/treatment/getalltreatmentes`, {
-//             params: {
-//                 page,
-//                 limit,
-//                 ...filters 
-//             }, headers })
-//         .then((response) => response)
-//         .catch((err) => err);
-// }
-
-// function deleteTreatment(id) {
-//     const headers = getHeaders(); 
-//     return axios
-//         .delete(`https://farm-project-bbzj.onrender.com/api/treatment/deletetreatment/${id}`, { headers })
-//         .then((response) => response)
-//         .catch((err) => err);
-// }
-
-// function getTreatmentByAnimal(page, limit, filters = {}) {
-//     const headers = getHeaders(); 
-//     return axios
-//         .get(`https://farm-project-bbzj.onrender.com/api/treatment/getAlltreatmentforAnimals`, {
-//             params: {
-//                 page,
-//                 limit,
-//                 ...filters 
-//             },
-//             headers })
-//         .then((response) => response)
-//         .catch((err) => err);
-// }
-
-// function deleteTreatmentByAnimal(id) {
-//     const headers = getHeaders(); 
-//     return axios
-//         .delete(`https://farm-project-bbzj.onrender.com/api/treatment/deletetreatmentforAnimals/${id}`, { headers })
-//         .then((response) => response)
-//         .catch((err) => err);
-// }
-
+    };
 
 function getLocationtMenue() {
     const headers = getHeaders(); 
@@ -76,7 +32,7 @@ function getVaccineMenue() {
         .catch((err) => err);
 }
 
-export default function LocationtContextProvider(props) {
+export default function LocationContextshedProvider(props) {
     return (
         <LocationContext.Provider value={{ getLocationtMenue,getVaccineMenue}}>
             {props.children}
