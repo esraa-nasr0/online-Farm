f223a206581e2e33130019bafc52548351028e60
esraa-nasr0 <esraaelbordeny@gmail.com>
2025-04-18 23:33:23 +0200

    new update in style login and register

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,30 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Sidebare from "../Sidebare/Sidebare";
 import "./Layout.css";
-import { useEffect } from "react";
-
 
 export default function Layout() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(window.innerWidth > 768);
+    const location = useLocation();
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
 
-    // useEffect(() => {
-    //     if (window.innerWidth <= 768) {
-    //         setIsSidebarOpen(false); // خليه مقفول على الموبايل
-    //     }
-    // }, []);
+    const shouldShowSidebar = !['/', '/home', '/register', '/login', '/dashboard','/forgetpassword','/verifyotp','/resetpassword'].includes(location.pathname);
+    // const shouldHideNavbar = ['/login', '/register'].includes(location.pathname);
+
+    useEffect(() => {
+        if (window.innerWidth <= 768) {
+            setIsSidebarOpen(false);
+        }
+    }, [location.pathname]);
 
     return (
         <div className="app-container">
-            <Navbar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+            
+                <Navbar 
+                    toggleSidebar={toggleSidebar} 
+                    isSidebarOpen={isSidebarOpen} 
+                />
+            
             <div className="content-wrapper">
-                <Sidebare isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-                <main className={`main-content ${isSidebarOpen ? "sidebar-open" : ""}`}>
+                {shouldShowSidebar && (
+                    <Sidebare 
+                        isOpen={isSidebarOpen} 
+                        toggleSidebar={toggleSidebar} 
+                    />
+                )}
+                <main className={`main-content ${isSidebarOpen && shouldShowSidebar ? "sidebar-open" : ""}`}>
                     <div className="content-container">
                         <Outlet />
                     </div>
